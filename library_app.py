ba0e75e14db8cc88a1e1512f73185692d112b3e1
N97XT <szymonbockowski97@gmail.com>
2019-10-05 13:59:36 +0200

    Trying to create dictionary from txt

---
+++ library_app.py
@@ -0,0 +1,23 @@
+# plik = open('users.txt')
+# print(plik.read())
+
+# for linia in plik:
+#     print(linia)
+
+def file_as_dict(file_path):
+   with open(file_path) as file_object:
+       lines = (line for line in file_object)
+       without_comments = remove_comments(lines)
+       stripped = (line.strip() for line in without_comments)
+       splitted = (line.split(None, 1) for line in stripped)
+       pairs = (entry for entry in splitted if len(entry) == 2)
+       return dict(pairs)
+
+def remove_comments(lines):
+   for line in lines:
+       if '#' in line:
+           yield line.split('#')[0]
+       else:
+           yield line
+
+print(file_as_dict('users.txt'))
